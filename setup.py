ff817ee2cd11c81002f2a6fd275b91fe52fb1194
Nathan Reynolds <email@nreynolds.co.uk>
2012-02-07 09:09:48 -0800

    Bump to v0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGE_PATH = 'src'
 setup(
     name='oscodepoint',
     url='https://github.com/nathforge/oscodepoint',
-    version='0.1',
+    version='0.2',
     description='A Python interface to Ordnance Survey\'s CodePoint-Open data',
     long_description=open('README.txt').read(),
     
